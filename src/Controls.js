6abecfb2db96646c4cacaf41052ef79ddc9fbddd
kritika243 <24kritikasharma24@gmail.com>
2021-11-10 16:52:07 +0530

    controls layout and styling

--- src/Controls.js
+++ src/Controls.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import ReactSlider from 'react-slider'
+import './slider.css'
 
 const Controls = () => {
   return (
     <div style={{ textAlign: 'left' }}>
       <label>Work for how many minutes?</label>
+      <ReactSlider
+        className={'slider'}
+        thumbClassName={'thumb'}
+        trackClassName={'track'}
+        value={45}
+        min={1}
+        max={120}
+      />
+
       <label>Need a break for few minutes ?</label>
+      <ReactSlider
+        className={'slider blue'}
+        thumbClassName={'thumb'}
+        trackClassName={'track'}
+        value={45}
+        min={1}
+        max={120}
+      />
     </div>
   )
 }
