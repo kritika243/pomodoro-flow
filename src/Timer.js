a6583e95c79ffb823633b5334b996ab42521fa5c
kritika243 <24kritikasharma24@gmail.com>
2021-11-10 15:51:27 +0530

    UI layout and buttons

--- src/Timer.js
+++ src/Timer.js
@@ -1,7 +1,34 @@
 import React from 'react'
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css'
+import PauseBtn from './PauseBtn'
+import PlayBtn from './PlayBtn'
+
+const color1 = '#ffb30f'
+const color2 = '#96cdff'
 
 const Timer = () => {
-  return <div>timer</div>
+  return (
+    <>
+      <div>
+        <CircularProgressbar
+          value={62}
+          text={`62%`}
+          styles={buildStyles({
+            rotation: 0.25,
+            strokeLinecap: 'butt',
+            textColor: 'white',
+            pathColor: color1,
+            trailColor: 'rgba(255, 255, 255, 0.562)',
+          })}
+        />
+      </div>
+      <div style={{ marginTop: '20px' }}>
+        <PlayBtn />
+        <PauseBtn />
+      </div>
+    </>
+  )
 }
 
 export default Timer
