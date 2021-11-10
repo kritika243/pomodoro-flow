29353eb3ca6d24ccbe506dbe2ff7068eb4307daa
kritika243 <24kritikasharma24@gmail.com>
2021-11-10 16:21:49 +0530

    Controls component started

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
+import { useState } from 'react'
 import './App.css'
+import Controls from './Controls'
 import Timer from './Timer'
 
 function App() {
-  return (
-    <main>
-      <Timer />
-    </main>
-  )
+  const [showControls, setShowControls] = useState(true)
+
+  return <main>{showControls ? <Controls /> : <Timer />}</main>
 }
 
 export default App
---
+++ src/Controls.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Controls = () => {
+  return (
+    <div style={{ textAlign: 'left' }}>
+      <label>Work for how many minutes?</label>
+      <label>Need a break for few minutes ?</label>
+    </div>
+  )
+}
+
+export default Controls
