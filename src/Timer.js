9aea9bec2c8b80c627e009f04b7f90d1309b5bb0
kritika243 <24kritikasharma24@gmail.com>
2021-11-17 22:02:15 +0530

    making the timer run

--- src/Timer.js
+++ src/Timer.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar'
 import 'react-circular-progressbar/dist/styles.css'
-import { useContext } from 'react'
+import { useContext, useState, useEffect, useRef } from 'react'
 import PauseBtn from './PauseBtn'
 import PlayBtn from './PlayBtn'
 import SettingsBtn from './SettingsBtn'
@@ -11,30 +11,80 @@ const color1 = '#ffb30f'
 const color2 = '#96cdff'
 
 const Timer = () => {
-  const settingsInfo = useContext(SettingsContext)
+  const controlsInfo = useContext(SettingsContext)
+  const [isPaused, setIsPaused] = useState(false)
+  const [mode, setMode] = useState('work') // work / break / null
+  const [secondsLeft, setSecondsLeft] = useState(0)
+
+  const secondsLeftRef = useRef(secondsLeft)
+  const isPausedRef = useRef(isPaused)
+  const modeRef = useRef(mode)
+
+  function tick() {
+    secondsLeftRef.current = secondsLeftRef.current - 1
+    setSecondsLeft(secondsLeftRef.current - 1)
+  }
+
+  function switchMode() {
+    const nextMode = modeRef === 'work' ? 'break' : 'work'
+    const nextSeconds =
+      nextMode === 'work'
+        ? controlsInfo.workMinutes * 60
+        : controlsInfo.breakMinutes * 60
+    setMode(nextMode)
+    modeRef.current = nextMode
+    setSecondsLeft(nextSeconds)
+    secondsLeftRef.current = nextSeconds
+  }
+
+  function initTimer() {
+    setSecondsLeft(controlsInfo.workMinutes * 60)
+  }
+
+  useEffect(() => {
+    initTimer()
+
+    const interval = setInterval(() => {
+      if (isPausedRef.current) return
+      if (secondsLeftRef.current === 0) {
+        return switchMode()
+      }
+      tick()
+    }, 1000)
+
+    return () => clearInterval(interval)
+  }, [controlsInfo])
+
+  const totalSeconds =
+    mode === 'work'
+      ? controlsInfo.workMinutes * 60
+      : controlsInfo.breakMinutes * 60
+  const percentage = Math.round((secondsLeft / totalSeconds) * 100)
+
+  const minutes = Math.floor(secondsLeft / 60)
+  let seconds = secondsLeft % 60
+  if (seconds < 10) seconds = '0' + seconds
+
   return (
     <>
       <div>
         <CircularProgressbar
-          value={62}
-          text={`62%`}
+          value={percentage}
+          text={minutes + ':' + seconds}
           styles={buildStyles({
-            rotation: 0.25,
-            strokeLinecap: 'butt',
             textColor: 'white',
-            pathColor: color1,
+            pathColor: mode === 'work' ? color1 : color2,
             trailColor: 'rgba(255, 255, 255, 0.562)',
           })}
         />
       </div>
       <div style={{ marginTop: '20px' }}>
-        <PlayBtn />
-        <PauseBtn />
+        {isPaused ? <PlayBtn /> : <PauseBtn />}
       </div>
       <div style={{ marginTop: '20px' }}>
         <SettingsBtn
           onClick={() => {
-            settingsInfo.setShowControls(true)
+            controlsInfo.setShowControls(true)
           }}
         />
       </div>
